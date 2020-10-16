8b1239f6d1c5b17795ca400cfa29740509770788
lukeliuli <lukeliuli@163.com>
2020-10-17 03:15:53 +0800

    Signed-off-by: lukeliuli <lukeliuli@163.com>

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ python调用Sumo,用于车联网教学
 * 具体为D:\oneDriveData\OneDrive\sumoWindowLibsumo\sumoSrcCodes\bin
 * 注意sumoWindowLibsumo是自己在window下编译的，并且带有libsumo功能 
 2. 第一个例子为采用python+TRACI 获得交通灯的信号和车的状态 
-* 具体为一辆车获取前方交通灯信号，根据当前交通灯状态，进行过绿灯车速引导。  
+* 具体为一辆车获取前方交通灯信号，根据当前交通灯状态，进行过绿灯车速引导。 
+* 注意代码中我加入了cProfile，用于分析代码运行时间
+ 
  
--- test1/testTraci1.py
+++ test1/testTraci1.py
@@ -5,6 +5,7 @@
 """
 
 
+import cProfile
 import os
 import sys
 
@@ -17,113 +18,114 @@ else:
 
 import traci
 
-# sys.path.append(os.path.join('c:', os.sep, 'whatever', 'path', 'to', 'sumo', 'tools'))
-print("preparing now \n")
+def test1():
 
+    print("preparing now \n")
+    path = os.getcwd()
+    path2 = path+"\sumoCfg2\my1LaneMap1NoVeh-server.sumocfg"
+    sumoBinary = "sumo-gui.exe"
+    # sumoBinary = "sumo.exe"
+    traci.start([sumoBinary, "-c", path2])
+    print("start now \n")
 
-path = os.getcwd()
-path2 = path+"\sumoCfg2\my1LaneMap1NoVeh-server.sumocfg"
-sumoBinary = "sumo-gui.exe"
-# sumoBinary = "sumo.exe"
-traci.start([sumoBinary, "-c", path2])
-print("start now \n")
-
-stepNum = 0
-requireStop = 0
-while requireStop == 0:
-    traci.simulationStep()
-
-    stepNum += 1
+    stepNum = 0
     requireStop = 0
-    timeT = traci.simulation.getTime()  # 当前时间
-    vehicles = traci.vehicle.getIDList()  # 当前在道路上的车辆
-    curTime = timeT
-    # for len(vehicles)>0:
-    #    traci.vehicle.setParameter("00001", "carFollowModel.SimpleCACC1", "Hi")
+    while requireStop == 0:
+        traci.simulationStep()
 
-    print("running "+str(curTime))
+        stepNum += 1
+        requireStop = 0
+        timeT = traci.simulation.getTime()  # 当前时间
+        vehicles = traci.vehicle.getIDList()  # 当前在道路上的车辆
+        curTime = timeT
+        # for len(vehicles)>0:
+        #    traci.vehicle.setParameter("00001", "carFollowModel.SimpleCACC1", "Hi")
 
-   # 每隔15秒加入1车，步长0.01
-    if stepNum % 1500 == 1:
-        vehSpeed = 0
-        vehPos = 10
-        vehLane = 0
-        vehichID = "veh"+str(stepNum)
-        traci.vehicle.addLegacy(
-            vehichID, "r1", -3, vehPos, vehSpeed, str(vehLane), "car1")
-        print("add a car "+vehichID+" at " + str(int(timeT))+"\n")
-    
-    
-    
-    
+        print("running "+str(curTime))
 
-    if timeT > 60:  # if time is over 200 second stoping the simulation
-        requireStop = 1
+    # 每隔15秒加入1车，步长0.01
+        if stepNum % 1500 == 1:
+            vehSpeed = 0
+            vehPos = 10
+            vehLane = 0
+            vehichID = "veh"+str(stepNum)
+            traci.vehicle.addLegacy(
+                vehichID, "r1", -3, vehPos, vehSpeed, str(vehLane), "car1")
+            print("add a car "+vehichID+" at " + str(int(timeT))+"\n")
+
+        if timeT > 60:  # if time is over 200 second stoping the simulation
+            requireStop = 1
+
+        #################################################################################
+        #
+        if len(vehicles) > 0:
+            for vehName in vehicles:
+                curspeed = traci.vehicle.getSpeed(vehName)
+                position = traci.vehicle.getPosition(vehName)
+                angle = traci.vehicle.getAngle(vehName)
+                latpos = traci.vehicle.getLateralLanePosition(vehName)
+                vehLatAlignment = traci.vehicle.getLateralAlignment(vehName)
+                vehNextTLS = traci.vehicle.getNextTLS(vehName)
+                laneID = traci.vehicle.getLaneID(vehName)
+                laneIndex = traci.vehicle.getLaneIndex(vehName)
+                roadId = traci.vehicle.getRoadID(vehName)
+
+                dist1 = traci.vehicle.getDistance(vehName)
+                drivingDist1 = traci.vehicle.getDrivingDistance(vehName, roadId, 0)
+
+                leader = traci.vehicle.getLeader(vehName)
+                leftLeader = traci.vehicle.getLeftLeaders(vehName)
+                leftFollower = traci.vehicle.getLeftFollowers(vehName)
+                rightFollowers = traci.vehicle.getRightFollowers(vehName)
+                rightLeaders = traci.vehicle.getRightLeaders(vehName)
+                slope = traci.vehicle.getSlope(vehName)
+                getAcceleration = traci.vehicle.getAcceleration(vehName)
+                fuelConsumption = traci.vehicle.getFuelConsumption(vehName)
+                lineInfo = traci.vehicle.getLine(vehName)
+                idEdges = traci.vehicle.getRoute(vehName)
+                routeID = traci.vehicle.getRouteID(vehName)
+                routeIndex = traci.vehicle.getRouteIndex(vehName)
+
+                # traci.vehicle.highlight(vehName)
+                nextStops = traci.vehicle.getNextStops(vehName)
+
+                # Return list of upcoming traffic lights [(tlsID, tlsIndex, distance, state), ...]
+                nextTLS = traci.vehicle.getNextTLS(vehName)
+                if len(nextTLS) == 0:
+                    continue
+
+                nextTLSID = nextTLS[0][0]
+                nextTLSDist = nextTLS[0][2]
+                states = traci.trafficlight.getRedYellowGreenState(nextTLSID)
+                nextSwitch = traci.trafficlight.getNextSwitch(nextTLSID)
+                phaseName = traci.trafficlight.getPhaseName(nextTLSID)
+                phase = traci.trafficlight.getPhase(nextTLSID)
+                phaseDuration = traci.trafficlight.getPhaseDuration(nextTLSID)
+                programLogic = traci.trafficlight.getProgram(nextTLSID)
+                controlledLinks = traci.trafficlight.getControlledLinks(nextTLSID)
+                allProgramLogics = traci.trafficlight.getAllProgramLogics(
+                    nextTLSID)
+                lgc1 = allProgramLogics[0]
+                greenDurTime = lgc1.phases[0].duration
+                yellowDurTime = lgc1.phases[1].duration
+                redDurTime = lgc1.phases[2].duration
+
+                phaseLeftTime = nextSwitch - timeT+0.00001
+                arrivalTimeNextTLS = nextTLSDist / (curspeed+0.000001)
+
+                if states == "GG":
+                    speedPassGreenTLSInGreen = nextTLSDist / phaseLeftTime
+
+                if states == "rr":
+                    speedPassGreenTLSInRed = nextTLSDist / (phaseLeftTime + 0.5)  # 加0.5秒
+
+                if states == "yy":
+                    speedPassYellowTLSInYellow = nextTLSDist / (phaseLeftTime)
+                    speedPassGreenTLSInYellow = nextTLSDist / (phaseLeftTime+redDurTime+0.5)
 
 
-    #################################################################################
-    #
-    if len(vehicles) > 0:
-        for vehName in vehicles:
-            curspeed = traci.vehicle.getSpeed(vehName)
-            position = traci.vehicle.getPosition(vehName)
-            angle = traci.vehicle.getAngle(vehName)
-            latpos = traci.vehicle.getLateralLanePosition(vehName)
-            vehLatAlignment = traci.vehicle.getLateralAlignment(vehName)
-            vehNextTLS = traci.vehicle.getNextTLS(vehName)
-            laneID = traci.vehicle.getLaneID(vehName)
-            laneIndex = traci.vehicle.getLaneIndex(vehName)
-            roadId = traci.vehicle.getRoadID(vehName)
+    traci.close()
 
-            dist1 = traci.vehicle.getDistance(vehName)
-            drivingDist1 = traci.vehicle.getDrivingDistance(vehName, roadId, 0)
 
-            leader = traci.vehicle.getLeader(vehName)
-            leftLeader = traci.vehicle.getLeftLeaders(vehName)
-            leftFollower = traci.vehicle.getLeftFollowers(vehName)
-            rightFollowers = traci.vehicle.getRightFollowers(vehName)
-            rightLeaders = traci.vehicle.getRightLeaders(vehName)
-            slope = traci.vehicle.getSlope(vehName)
-            getAcceleration = traci.vehicle.getAcceleration(vehName)
-            fuelConsumption = traci.vehicle.getFuelConsumption(vehName)
-            lineInfo = traci.vehicle.getLine(vehName)
-            idEdges = traci.vehicle.getRoute(vehName)
-            routeID = traci.vehicle.getRouteID(vehName)
-            routeIndex = traci.vehicle.getRouteIndex(vehName)
-
-            # traci.vehicle.highlight(vehName)
-            nextStops = traci.vehicle.getNextStops(vehName)
-            # Return list of upcoming traffic lights [(tlsID, tlsIndex, distance, state), ...]
-            nextTLS = traci.vehicle.getNextTLS(vehName)
-            nextTLSID = nextTLS[0][0]
-            nextTLSDist = nextTLS[0][2]
-            states = traci.trafficlight.getRedYellowGreenState(nextTLSID)
-            nextSwitch = traci.trafficlight.getNextSwitch(nextTLSID)
-            phaseName = traci.trafficlight.getPhaseName(nextTLSID)
-            phase = traci.trafficlight.getPhase(nextTLSID)
-            phaseDuration = traci.trafficlight.getPhaseDuration(nextTLSID)
-            programLogic = traci.trafficlight.getProgram(nextTLSID)
-            controlledLinks = traci.trafficlight.getControlledLinks(nextTLSID)
-            allProgramLogics = traci.trafficlight.getAllProgramLogics(nextTLSID)
-            lgc1 = allProgramLogics[0]
-            greenDurTime = lgc1.phases[0].duration
-            yellowDurTime = lgc1.phases[1].duration
-            redDurTime = lgc1.phases[2].duration
-
-            phaseLeftTime = nextSwitch - timeT+0.00001
-            arrivalTimeNextTLS = nextTLSDist / (curspeed+0.000001)
-
-            if states == "GG":
-                speedPassGreenTLSInGreen = nextTLSDist / phaseLeftTime
-            
-            if states == "rr":
-                speedPassGreenTLSInRed = nextTLSDist / (phaseLeftTime + 0.5)  #加0.5秒
-
-            if states == "yy":
-                speedPassYellowTLSInYellow = nextTLSDist / (phaseLeftTime)
-                speedPassGreenTLSInYellow = nextTLSDist / (phaseLeftTime+redDurTime+0.5)
-
-  
-    
-
-traci.close()
+if __name__ == '__main__':
+   cProfile.run('test1()')
